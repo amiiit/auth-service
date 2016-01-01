7e3ef4ee5d5a5f4dbf6cfc854500ecd3685683d2
Amit Jakubowicz <jakubowicz.amit@gmail.com>
2016-01-01 14:54:51 +0100

    start tests after db connection

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,16 +1,23 @@
 
-let context = require.context('./src', true, /.+\.spec\.js?$/);
-
 require('core-js/es5');
 
 var MongoClient = require('mongodb').MongoClient
-  , Server = require('mongodb').Server;
+    , Server = require('mongodb').Server;
 
 var assert = require('assert')
 
-MongoClient.connect('mongodb://192.168.64.2/auth_test', function(err, db) {
-  console.log('tests: new connection set')
+
+before(function(done){
+    MongoClient.connect('mongodb://192.168.64.2/auth_test', function (err, db) {
+        if (err) {
+            reject(err)
+        } else {
+            console.log('tests: new connection set', db.toString())
+            done()
+        }
+    })
 })
 
-// context.keys().forEach(context);
-// module.exports = context;
+let context = require.context('./src', true, /.+\.spec\.js?$/);
+context.keys().forEach(context);
+module.exports = context;
