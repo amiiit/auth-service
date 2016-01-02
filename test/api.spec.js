685ac57214f7803745290e37694c0fce4a92c709
Amit Jakubowicz <jakubowicz.amit@gmail.com>
2016-01-02 09:26:36 +0100

    db wired through tests

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,22 +1,25 @@
 import TestUtils from './test-utils'
 import AuthService from 'auth/service'
+import request from 'supertest'
 
 var expect = require('chai').expect
 
-let service
-let testUtils = new TestUtils()
+let service, db, app
+testUtils = new TestUtils()
 
 describe('Signup', function () {
 
-    before(function(){
+    before(function () {
         service = new AuthService({email: {}, dbUrl: testUtils.dbUrl})
         service.init()
+        db = testUtils.dbConnection
     })
 
-    describe('#max', function () {
-        it('returns the biggest number from the arguments', function () {
-            var max = Math.max(1, 2, 10, 3);
-            expect(max).to.equal(10);
+    describe('singup request', function () {
+        it('adds entry in db', function () {
+            request(app)
+                .post('/signup')
+                .field('email', 'me@b.com')
         });
     });
 
